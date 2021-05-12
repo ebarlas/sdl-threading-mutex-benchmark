831067651355b677e98707b5efc10e8f38de272a
Elliot Barlas <elliotbarlas@gmail.com>
2021-05-12 14:15:17 -0700

    Initial version of C++ SDL multi-threading-with-mutex benchmark tool.

---
+++ README.md
@@ -0,0 +1,76 @@
+# Overview
+
+This repository contains a simple C++ SDL2 application for gauging the overhead of multi-threading
+with mutexes.
+
+What is the frame-rate penalty incurred by moving model updates out of the main render thread and into an 
+update thread where model reads and writes are mutex-protected?
+
+This particular experiment is set up to surface the costs associated with multi-threading in this manner 
+and doesn't highlight the benefits or motivations for such a design. The model updates performed in the
+tests are rectangle (x, y) position increments. Just enough to display an animation but not enough
+to register on the performance scale.
+
+# Methodology
+
+The benchmark application animates particles streaming across the display window. 
+A particle is a small pixelated rectangle. Particles are updated at a fixed target rate.
+
+![Results](docs/display.png)
+
+The application can be broken into several parts: 
+
+* Model update
+  * Particle positions are incremented
+* Model read
+  * Particle positions are read by render thread
+* Model Render
+  * Particles are rendered with `SDL_RenderFillRects`
+
+There are three test modes:
+
+* Single-thread no mutexes
+  * Updates happen inline, based on observed time deltas
+* Multi-threaded mutex per particle
+  * Updates happen in dedicated update threads
+  * Each particle position is _updated_ within a granular mutex section by the update thread
+  * Each particle position is _read_ within a granular mutex section by the render thread
+* Multi-threaded single mutex
+  * Updates happen in dedicated update threads
+  * All particle position are _updated_ within a single broad mutex section by the update thread
+  * All particle positions are _read_ within a single broad mutex section by the render thread
+
+# Results
+
+The results below were gathered on the following machine:
+
+* MacBook Pro
+* macOS Catalina 10.15.7
+* 2.4 GHz 8-Core Intel Core i9 processor
+* 32 GB 2667 MHz DDR4
+
+Test parameters:
+
+* Window size: 640 x 640
+* Individual test run time: 5 seconds
+* Target update rate: 200 updates per second
+* Particle size: 3 x 3
+* Test runs per parameter group: 4
+* Particle counts: 100, 1,000, 5,000, 10,000, 20,000, 50,000
+
+Of the _N_ runs per parameter group, the lowest FPS result is discarded and the remaining _N - 1_ 
+FPS results are averaged.
+
+|Test|100|1,000|5,000|10,000|20,000|50,000|
+|------|---|-----|-----|------|------|------|
+|Single-Threaded No Mutexes|2203|2205|2321|2187|1956|958|
+|Multi-Threaded Mutex Per Rect|2232|2103|2133|1548|856|383|
+|Multi-Threaded Single Mutex|2201|2161|2256|2137|1791|881|
+
+![Results](docs/sdl-thread-mutex-results.png)
+
+# Conclusion
+
+* Below a threshold (roughly 5,000 particles in the tests conducted) overhead is minimal and performance is comparable across all test modes.
+* At scale, the overhead of a granular mutex per particle is considerable.
+* The overhead of a single mutex is quite small, though still measurable.
---
+++ benchmark.cpp
@@ -0,0 +1,248 @@
+#include <functional>
+#include <vector>
+#include <numeric>
+#include "SDL.h"
+
+struct Runner {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    int width;
+    int height;
+    int duration;
+    bool quit;
+
+    Runner(int width, int height, int duration) : width(width), height(height), duration(duration), quit(false) {
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+            SDL_Log("SDL could not initialize. SDL Error: %s", SDL_GetError());
+            std::terminate();
+        }
+
+        window = SDL_CreateWindow(
+                "Benchmark",
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                width,
+                height,
+                SDL_WINDOW_SHOWN);
+        if (window == nullptr) {
+            SDL_Log("Window could not be created. SDL Error: %s", SDL_GetError());
+            std::terminate();
+        }
+
+        renderer = SDL_CreateRenderer(
+                window,
+                -1,
+                0);
+        if (renderer == nullptr) {
+            SDL_Log("Renderer could not be created. SDL Error: %s", SDL_GetError());
+            std::terminate();
+        }
+    }
+
+    ~Runner() {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+    int run(std::function<void(SDL_Renderer *)> fn) {
+        Uint32 deadline = SDL_GetTicks() + duration;
+        int count = 0;
+        while (!quit && SDL_GetTicks() < deadline) {
+            SDL_Event e;
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }
+            }
+            fn(renderer);
+            SDL_RenderPresent(renderer);
+            count++;
+        }
+        return count / (duration / 1000);
+    }
+};
+
+int runFunction(void *data) {
+    auto fn = (std::function<void()> *) data;
+    (*fn)();
+    return 0;
+}
+
+void fillRects(SDL_Renderer *ren, SDL_Rect *rects, int numRects) {
+    SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+    SDL_RenderClear(ren);
+    SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+    SDL_RenderFillRects(ren, rects, numRects);
+}
+
+SDL_Rect randomRect(SDL_Point size, int sideLen) {
+    return {std::rand() % (size.x - sideLen), std::rand() % (size.y - sideLen), sideLen, sideLen};
+}
+
+void moveRect(SDL_Rect &rect, SDL_Point &size) {
+    rect.x = (rect.x + 1) % size.x;
+    rect.y = (rect.y + 1) % size.y;
+}
+
+int runSingleThreadNoMutex(Runner &runner, int numRects, int updatePeriod, int sideLen) {
+    SDL_Point size{runner.width, runner.height};
+
+    auto rects = new SDL_Rect[numRects];
+    for (int i = 0; i < numRects; i++) {
+        rects[i] = randomRect(size, sideLen);
+    }
+
+    Uint32 updateTime = 0;
+    auto fn = [&size, rects, numRects, updatePeriod, &updateTime](SDL_Renderer *ren) {
+        Uint32 now = SDL_GetTicks();
+        if (now - updateTime >= updatePeriod) {
+            for (int i = 0; i < numRects; i++) {
+                moveRect(rects[i], size);
+            }
+            updateTime = now;
+        }
+        fillRects(ren, rects, numRects);
+    };
+    int fps = runner.run(fn);
+
+    delete[]rects;
+
+    return fps;
+}
+
+int runMultiThreadedMultiMutex(Runner &runner, int numRects, int updatePeriod, int sideLen) {
+    SDL_Point size{runner.width, runner.height};
+
+    auto updateRects = new SDL_Rect[numRects];
+    auto mutexes = new void *[numRects];
+    for (int i = 0; i < numRects; i++) {
+        updateRects[i] = randomRect(size, sideLen);
+        mutexes[i] = SDL_CreateMutex();
+    }
+
+    SDL_atomic_t quit;
+    SDL_AtomicSet(&quit, 0);
+
+    std::function<void()> updateFn = [&size, updateRects, mutexes, numRects, updatePeriod, &quit]() {
+        while (!SDL_AtomicGet(&quit)) {
+            for (int i = 0; i < numRects; i++) {
+                auto mutex = static_cast<SDL_mutex *>(mutexes[i]);
+                SDL_LockMutex(mutex);
+                moveRect(updateRects[i], size);
+                SDL_UnlockMutex(mutex);
+            }
+            SDL_Delay(updatePeriod);
+        }
+    };
+
+    SDL_Thread *thread = SDL_CreateThread(runFunction, "update-thread", &updateFn);
+
+    auto renderRects = new SDL_Rect[numRects];
+
+    auto renderFn = [updateRects, renderRects, mutexes, numRects](SDL_Renderer *ren) {
+        for (int i = 0; i < numRects; i++) {
+            auto mutex = static_cast<SDL_mutex *>(mutexes[i]);
+            SDL_LockMutex(mutex);
+            renderRects[i] = updateRects[i];
+            SDL_UnlockMutex(mutex);
+        }
+        fillRects(ren, renderRects, numRects);
+    };
+    int fps = runner.run(renderFn);
+
+    int status;
+    SDL_AtomicSet(&quit, 1);
+    SDL_WaitThread(thread, &status);
+
+    for (int i = 0; i < numRects; i++) {
+        SDL_DestroyMutex(static_cast<SDL_mutex *>(mutexes[i]));
+    }
+
+    delete[]updateRects;
+    delete[]mutexes;
+    delete[]renderRects;
+
+    return fps;
+}
+
+int runMultiThreadedSingleMutex(Runner &runner, int numRects, int updatePeriod, int sideLen) {
+    SDL_Point size{runner.width, runner.height};
+
+    auto updateRects = new SDL_Rect[numRects];
+    auto mutex = SDL_CreateMutex();
+    for (int i = 0; i < numRects; i++) {
+        updateRects[i] = randomRect(size, sideLen);
+    }
+
+    SDL_atomic_t quit;
+    SDL_AtomicSet(&quit, 0);
+
+    std::function<void()> updateFn = [&size, updateRects, mutex, numRects, updatePeriod, &quit]() {
+        while (!SDL_AtomicGet(&quit)) {
+            SDL_LockMutex(mutex);
+            for (int i = 0; i < numRects; i++) {
+                moveRect(updateRects[i], size);
+            }
+            SDL_UnlockMutex(mutex);
+            SDL_Delay(updatePeriod);
+        }
+    };
+
+    SDL_Thread *thread = SDL_CreateThread(runFunction, "update-thread", &updateFn);
+
+    auto renderRects = new SDL_Rect[numRects];
+
+    auto renderFn = [updateRects, renderRects, mutex, numRects](SDL_Renderer *ren) {
+        SDL_LockMutex(mutex);
+        for (int i = 0; i < numRects; i++) {
+            renderRects[i] = updateRects[i];
+        }
+        SDL_UnlockMutex(mutex);
+        fillRects(ren, renderRects, numRects);
+    };
+    int fps = runner.run(renderFn);
+
+    int status;
+    SDL_AtomicSet(&quit, 1);
+    SDL_WaitThread(thread, &status);
+
+    SDL_DestroyMutex(mutex);
+
+    delete[]updateRects;
+    delete[]renderRects;
+
+    return fps;
+}
+
+int avgMinExcluded(std::vector<int> &vals) {
+    std::sort(vals.begin(), vals.end());
+    return std::accumulate(vals.begin() + 1, vals.end(), 0) / (vals.size() - 1);
+}
+
+int main() {
+    int width = 640;
+    int height = 640;
+    int duration = 5'000;
+    int updatePeriod = 5;
+    int sideLen = 3;
+    int runs = 4;
+    int numRectsArr[] = {100, 1'000, 5'000, 10'000, 20'000, 50'000};
+    auto fns = {runSingleThreadNoMutex, runMultiThreadedMultiMutex, runMultiThreadedSingleMutex};
+
+    Runner runner(width, height, duration);
+
+    for (auto fn : fns) {
+        for (int numRects : numRectsArr) {
+            std::vector<int> fpsvec;
+            for (int i = 0; i < runs; i++) {
+                int fps = fn(runner, numRects, updatePeriod, sideLen);
+                fpsvec.push_back(fps);
+                SDL_Log("i=%d, numRects=%d, fps=%d", i, numRects, fps);
+            }
+            SDL_Log("numRects=%d, avgFps=%d", numRects, avgMinExcluded(fpsvec));
+        }
+    }
+
+    return 0;
+}
